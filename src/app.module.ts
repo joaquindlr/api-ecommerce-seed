9eea5199492bae68540b644949d270f62ee8d68d
joaquin de los reyes <joaquindr2011@gmail.com>
2023-04-12 15:28:28 -0300

    add products module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
+import { ProductsModule } from './products/products.module';
 import TypeOrmConfigImport from './config/TypeOrmConfig';
 
 @Module({
-  imports: [TypeOrmConfigImport, UsersModule, AuthModule],
+  imports: [TypeOrmConfigImport, UsersModule, AuthModule, ProductsModule],
   controllers: [AppController],
   providers: [AppService],
 })
